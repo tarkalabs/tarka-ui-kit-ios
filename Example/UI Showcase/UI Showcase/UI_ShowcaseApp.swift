5f111cff4561a2f69b4ba1cb829bcc46dce6c603
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-04-07 11:21:13 +0530

    Created package and example app

---
+++ Example/UI Showcase/UI Showcase/UI_ShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_ShowcaseApp.swift
+//  UI Showcase
+//
+//  Created by Arvindh Sukumar on 07/04/23.
+//
+
+import SwiftUI
+
+@main
+struct UI_ShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
