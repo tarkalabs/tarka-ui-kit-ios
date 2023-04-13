49ca7981cfcd0ca09ad7aab05978157ec3d1b40b
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-04-13 15:02:30 +0530

    More indentation

--- Example/UI Showcase/UI Showcase/UI_ShowcaseApp.swift
+++ Example/UI Showcase/UI Showcase/UI_ShowcaseApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct UI_ShowcaseApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
