2b2827be76f6830929a4715b8caa9442de13a567
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-04-13 15:02:30 +0530

    More indentation

--- Example/UI Showcase/UI Showcase/ContentView.swift
+++ Example/UI Showcase/UI Showcase/ContentView.swift
@@ -5,23 +5,23 @@
 //  Created by Arvindh Sukumar on 07/04/23.
 //
 
-import SwiftUI
 import EAMUI
+import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")        
     }
+    .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
