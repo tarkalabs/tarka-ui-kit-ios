0aa5864e4a8e0c0c64ba559e8171d7c844346026
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-04-18 14:01:54 +0530

    Added EAMSymbol enum cases needed for settings

---
+++ Sources/EAMUI/Styles/Symbols.swift
@@ -0,0 +1,29 @@
+//
+//  Symbols.swift
+//  
+//
+//  Created by Arvindh Sukumar on 18/04/23.
+//
+
+import SwiftUI
+
+public enum EAMSymbol: String {
+  case chevronDown
+  case chevronRight
+  case checkmarkStarburst
+  case copy
+  case delete
+  case dismiss
+  case document
+  case documentDownload
+  case documentError
+  case export
+  case map
+  case questionCircle
+  case refresh
+  case reorderDots
+  case search
+  case shield
+  case sync
+  case tabs
+}
