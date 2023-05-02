fb83f938f0ca95b164be0133a394f3c4c891059b
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-05-02 12:02:48 +0530

    Created IconButton

---
+++ Sources/EAMUI/Components/Base/IconButton.swift
@@ -0,0 +1,157 @@
+//
+//  IconButton.swift
+//  
+//
+//  Created by Arvindh Sukumar on 02/05/23.
+//
+
+import SwiftUI
+
+public enum IconButtonStyle {
+  case outline, ghost, secondary, primary
+}
+
+public enum IconButtonSize {
+  case xs, s, m, l, xl
+}
+
+public struct IconButton: View {
+  @ObservedObject private var viewModel: IconButtonViewModel = IconButtonViewModel()
+  public var icon: EAMSymbol
+  public var action: () -> Void
+  
+  public init(icon: EAMSymbol, action: @escaping () -> Void) {
+    self.icon = icon
+    self.action = action
+  }
+  
+  public var body: some View {
+    Button {
+      action()
+    } label: {
+      iconView
+    }
+    .frame(
+      width: viewModel.buttonSize.width,
+      height: viewModel.buttonSize.height
+    )
+    .contentShape(Circle()) // So that only the circular portion is tappable
+    .clipShape(Circle())
+    .background(backgroundView)
+    .overlay(content: borderView)
+  }
+  
+  @ViewBuilder
+  private var iconView: some View {
+    Image(icon)
+      .resizable()
+      .scaledToFit()
+      .frame(
+        width: viewModel.iconSize.width,
+        height: viewModel.iconSize.height
+      )
+      .foregroundColor(viewModel.iconColor)
+      .frame(
+        maxWidth: .infinity,
+        maxHeight: .infinity
+      ) // So that the entire button is tappable, not just the icon
+  }
+  
+  @ViewBuilder
+  private var backgroundView: some View {
+    viewModel.backgroundColor
+  }
+  
+  @ViewBuilder
+  private func borderView() -> some View {
+    Circle()
+      .stroke(
+        viewModel.borderColor,
+        style: StrokeStyle(
+          lineWidth: 1.5
+        )
+      )
+  }
+  
+  public func iconButtonStyle(_ style: IconButtonStyle) -> Self {
+    viewModel.style = style
+    return self
+  }
+  
+  public func iconButtonSize(_ size: IconButtonSize) -> Self {
+    viewModel.size = size
+    return self
+  }
+}
+
+private class IconButtonViewModel: ObservableObject {
+  @Published var style: IconButtonStyle = .ghost
+  @Published var size: IconButtonSize = .s
+  
+  var iconColor: Color {
+    switch style {
+    case .outline, .ghost:
+      return .onSurface
+    case .secondary:
+      return .onSecondary
+    case .primary:
+      return .onPrimary
+    }
+  }
+  
+  var backgroundColor: Color {
+    switch style {
+    case .outline, .ghost:
+      return .clear
+    case .secondary:
+      return .secondaryEAM
+    case .primary:
+      return .primaryEAM
+    }
+  }
+  
+  var borderColor: Color {
+    switch style {
+    case .ghost, .secondary, .primary:
+      return backgroundColor
+    case .outline:
+      return .outline
+    }
+  }
+  
+  var buttonSize: CGSize {
+    switch size {
+    case .xs:
+      return CGSize(width: 20, height: 20)
+    case .s:
+      return CGSize(width: 24, height: 24)
+    case .m:
+      return CGSize(width: 32, height: 32)
+    case .l:
+      return CGSize(width: 40, height: 40)
+    case .xl:
+      return CGSize(width: 48, height: 48)
+    }
+  }
+  
+  var iconSize: CGSize {
+    switch size {
+    case .xs, .s:
+      return CGSize(width: 12, height: 12)
+    case .m, .l, .xl:
+      return CGSize(width: 24, height: 24)
+    }
+  }
+}
+
+struct IconButtonView_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      IconButton(icon: .chevronDown, action: {
+        
+      })
+      .iconButtonStyle(.primary)
+      .iconButtonSize(.l)
+    }
+  }
+}
