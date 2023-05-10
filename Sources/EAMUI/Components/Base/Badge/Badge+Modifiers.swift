797c1304456f41c283cf0bfac716394e271550b6
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-05-10 12:00:44 +0530

    Made badgeSize an environment value

---
+++ Sources/EAMUI/Components/Base/Badge/Badge+Modifiers.swift
@@ -0,0 +1,21 @@
+//
+//  Badge+Modifiers.swift
+//  
+//
+//  Created by Arvindh Sukumar on 10/05/23.
+//
+
+import SwiftUI
+
+extension EnvironmentValues {
+  var badgeSize: BadgeSize {
+    get { self[BadgeSize.self] }
+    set { self[BadgeSize.self] = newValue }
+  }
+}
+
+public extension View {
+  func badgeSize(_ size: BadgeSize) -> some View {
+    environment(\.badgeSize, size)
+  }
+}
