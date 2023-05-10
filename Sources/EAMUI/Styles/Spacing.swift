8dbf93324f24075052902e86ecb5ca73c2c8d8c6
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-05-10 13:20:59 +0530

    Using implicit self in Spacing

--- Sources/EAMUI/Styles/Spacing.swift
+++ Sources/EAMUI/Styles/Spacing.swift
@@ -11,22 +11,22 @@ public enum Spacing {
   public static let none: CGFloat = 0
   
   public static let baseVertical: CGFloat = 4
-  public static let doubleVertical: CGFloat = Spacing.baseVertical * 2
-  public static let halfVertical: CGFloat = Spacing.baseVertical / 2
+  public static let doubleVertical: CGFloat = baseVertical * 2
+  public static let halfVertical: CGFloat = baseVertical / 2
   
   public static let baseHorizontal: CGFloat = 16
-  public static let halfHorizontal: CGFloat = Spacing.baseHorizontal / 2
+  public static let halfHorizontal: CGFloat = baseHorizontal / 2
   
   public static func custom(_ spacing: CGFloat) -> CGFloat {
     spacing
   }
   
   public static func verticalMultiple(_ multiplier: CGFloat) -> CGFloat {
-    Spacing.baseVertical * multiplier
+    baseVertical * multiplier
   }
   
   public static func horizontalMultiple(_ multiplier: CGFloat) -> CGFloat {
-    Spacing.baseHorizontal * multiplier
+    baseHorizontal * multiplier
   }
 
 }
