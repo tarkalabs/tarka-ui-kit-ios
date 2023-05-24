61b5c8ecb5ec3013c7b820f12d8d73c2438f69cc
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-05-24 13:09:23 +0530

    Added SnackBar documentation

--- Sources/TarkaUI/Components/SnackBar/SnackBar.swift
+++ Sources/TarkaUI/Components/SnackBar/SnackBar.swift
@@ -47,10 +47,29 @@ public enum SnackBarStyle: EnvironmentKey {
   }
 }
 
+/// A view that displays a message for a brief period of time and then disappears automatically.
+///
+/// Use a snackbar to display a brief message at the bottom of the screen. Snackbars appear above all other elements on screen and only one can be displayed at a time.
+///
+/// Parameters:
+/// - message: The message to display in the snackbar.
+///
+/// Example:
+/// ```
+/// SnackBar(message: "This is a snackbar")
+///  .snackBarStyle(.success)
+/// ```
+///
+
 public struct SnackBar: View {
   @Environment(\.snackBarStyle) var style
   var message: any StringProtocol
   
+  /// Creates a snackbar with the specified message.
+  ///
+  /// - Parameters:
+  ///   - message: The message to display in the snackbar.
+  ///
   public init(message: any StringProtocol) {
     self.message = message
   }
