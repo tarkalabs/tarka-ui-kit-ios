ff8254bf20f658921ae6bca30d8b2fbfb0ba3ee4
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-05-24 11:30:49 +0530

    Made NavigationRow support any accessory view

--- Sources/TarkaUI/Components/NavigationRow/NavigationRow.swift
+++ Sources/TarkaUI/Components/NavigationRow/NavigationRow.swift
@@ -9,39 +9,45 @@ import SwiftUI
 
 /// A view that displays a navigation row with an optional symbol and badge count.
 ///
-/// The navigation row is a horizontal stack with an optional symbol, title and badge count.
+/// The navigation row is a horizontal stack with an optional symbol, title and an optional extra view.
 ///
 /// Example usage:
 ///
 ///     NavigationRow(
 ///       title: "Label",
 ///       symbol: .export,
-///       badgeCount: 100
+///       accessoryView: {
+///         Badge()
+///       }
 ///     )
 ///
 /// - Parameters:
 ///   - title: The title to display in the navigation row.
 ///   - symbol: The symbol to display in the navigation row. The default value is `nil`.
-///   - badgeCount: The badge count to display in the navigation row. The default value is `nil`.
+///   - accessoryView: Extra content to display in the navigation row
 ///
-public struct NavigationRow: View {
+public struct NavigationRow<Content>: View where Content: View {
   var title: any StringProtocol
   var symbol: Icon?
-  var badgeCount: Int?
+  var accessoryView: () -> Content
   
   @Environment(\.detailDisclosure) private var showDetailDisclosure
   
-  /// Creates a navigation row with the specified title, symbol and badge count.
+  /// Creates a navigation row with the specified title, symbol and an extra view.
   ///
   /// - Parameters:
   ///   - title: The title to display in the navigation row.
   ///   - symbol: The symbol to display in the navigation row. The default value is `nil`.
-  ///   - badgeCount: The badge count to display in the navigation row. The default value is `nil`.
+  ///   - accessoryView: Extra content to display in the navigation row.
   ///
-  public init(title: any StringProtocol, symbol: Icon? = nil, badgeCount: Int? = nil) {
+  public init(
+    title: any StringProtocol,
+    symbol: Icon? = nil,
+    @ViewBuilder _ accessoryView: @escaping () -> Content = { EmptyView() }
+  ) {
     self.title = title
     self.symbol = symbol
-    self.badgeCount = badgeCount
+    self.accessoryView = accessoryView
   }
   
   public var body: some View {
@@ -59,9 +65,7 @@ public struct NavigationRow: View {
       
       Spacer()
       
-      if let badgeCount = badgeCount {
-        Badge(count: badgeCount) .badgeSize(.m)
-      }
+      accessoryView()
       
       if showDetailDisclosure {
         DetailDisclosure()
@@ -82,9 +86,13 @@ public struct NavigationRow: View {
 struct NavigationRow_Previews: PreviewProvider {
   static var previews: some View {
     Group {
-      NavigationRow(title: "Label", symbol: Symbol.reorderDots, badgeCount: 100)
-      NavigationRow(title: "Label", symbol: Symbol.reorderDots, badgeCount: 100)
-        .detailDisclosure()
+      NavigationRow(title: "Label", symbol: Symbol.reorderDots) {
+        Badge(count: 100)
+      }
+      NavigationRow(title: "Label", symbol: Symbol.reorderDots) {
+        Badge(count: 100)
+      }
+      .detailDisclosure()
     }
   }
 }
