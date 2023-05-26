c3f6604651f76b8396de7bd31e768f9246bd01ef
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-05-26 12:04:01 +0530

    Added prefix to ButtonBlock

--- Sources/TarkaUI/Components/ButtonBlock/TUIButtonBlockAction.swift
+++ Sources/TarkaUI/Components/ButtonBlock/TUIButtonBlockAction.swift
@@ -1,5 +1,5 @@
 //
-//  ButtonBlockAction.swift
+//  TUIButtonBlockAction.swift
 //  
 //
 //  Created by Arvindh Sukumar on 02/05/23.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-/// The `ButtonBlockAction` protocol defines the properties and methods required for a button block action.
+/// The `TUIButtonBlockAction` protocol defines the properties and methods required for a button block action.
 /// 
 /// - Parameters:
 ///   - id: A unique identifier for the action. Needed for iterating over the actions in a `ForEach` loop.
@@ -16,7 +16,7 @@ import Foundation
 ///   - rightIcon: The right icon of the action.
 ///   - handler: The action to perform when the user taps the button.
 ///
-public protocol ButtonBlockAction {
+public protocol TUIButtonBlockAction {
   var id: String { get }
   var title: String { get }
   var leftIcon: Icon? { get }
@@ -24,10 +24,10 @@ public protocol ButtonBlockAction {
   var handler: () -> Void { get }
 }
 
-/// The `ButtonBlockActionBuilder` result builder is used to build an array of `ButtonBlockAction` instances.
+/// The `ButtonBlockActionBuilder` result builder is used to build an array of `TUIButtonBlockAction` instances.
 @resultBuilder
 public struct ButtonBlockActionBuilder {
-  public static func buildBlock(_ components: any ButtonBlockAction...) -> [any ButtonBlockAction] {
+  public static func buildBlock(_ components: any TUIButtonBlockAction...) -> [any TUIButtonBlockAction] {
     Array(components)
   }
 }
