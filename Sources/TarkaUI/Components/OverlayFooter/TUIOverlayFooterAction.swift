4e6c59d4e1685777c225470b31f05ace087eed96
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-05-26 12:09:07 +0530

    Added prefix to OverlayFooter

--- Sources/TarkaUI/Components/OverlayFooter/TUIOverlayFooterAction.swift
+++ Sources/TarkaUI/Components/OverlayFooter/TUIOverlayFooterAction.swift
@@ -1,5 +1,5 @@
 //
-//  OverlayFooterAction.swift
+//  TUIOverlayFooterAction.swift
 //  
 //
 //  Created by Arvindh Sukumar on 05/05/23.
@@ -7,23 +7,23 @@
 
 import Foundation
 
-/// The `OverlayFooterAction` protocol defines the properties and methods required for an overlay footer action.
+/// The `TUIOverlayFooterAction` protocol defines the properties and methods required for an overlay footer action.
 ///
 /// - Parameters:
 ///   - id: A unique identifier for the action. Needed for iterating over the actions in a `ForEach` loop.
 ///   - icon: The icon of the action.
 ///   - handler: The action to perform when the user taps the action.
 ///
-public protocol OverlayFooterAction {
+public protocol TUIOverlayFooterAction {
   var id: String { get }
   var icon: Icon { get }
   var handler: () -> Void { get }
 }
 
-/// The `OverlayFooterActionBuilder` result builder is used to build an array of `OverlayFooterAction` instances.
+/// The `OverlayFooterActionBuilder` result builder is used to build an array of `TUIOverlayFooterAction` instances.
 @resultBuilder
 public struct OverlayFooterActionBuilder {
-  public static func buildBlock(_ components: any OverlayFooterAction...) -> [any OverlayFooterAction] {
+  public static func buildBlock(_ components: any TUIOverlayFooterAction...) -> [any TUIOverlayFooterAction] {
     Array(components)
   }
 }
