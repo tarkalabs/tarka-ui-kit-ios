557739c233d45ce9b6b55c84ca0ebc4ad73b8bfb
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2023-05-26 11:59:37 +0530

    Added prefix to DetailDisclosure

--- Sources/TarkaUI/Components/TextRow/TextRow.swift
+++ Sources/TarkaUI/Components/TextRow/TextRow.swift
@@ -39,7 +39,7 @@ public struct TextRow: View {
       if showDetailDisclosure {
         Spacer()
         
-        DetailDisclosure()
+        TUIDetailDisclosure()
       }
     }
     .frame(maxWidth: .infinity, alignment: .leading)
