a7ef84ab795509d5d70544924e3a1fb759520862
Maheshwaran <102943217+maheshwaran01m@users.noreply.github.com>
2023-06-23 12:41:53 +0530

    Created TUISelectionRow (#13)
    * TUISelectionRow
     - Created selectionRow
    
    * TUISelectionRow
     - Work around for listView to match figma design
    
    * TUISelectionRow
    - Indentation Fix
    
    * TUISelectionRow
     - Updated preview naming
    
    * TUISelectionRow
     - Updated with generic to accept any swiftUI to show the selection
    
    * TUISelectionRow
    - Updated Padding
    
    * Symbol
     - Created checkbox symbol
    
    * TUISelectionRow
     - Updated maxWidth
    
    * TUISelectionRow
     - Updated with isSelected bool value
    
    * TUISelectionRow
     - updated comments

---
+++ Sources/TarkaUI/Components/SelectionRow/TUISelectionRow.swift
@@ -0,0 +1,72 @@
+//
+//  TUISelectionRow.swift
+//  
+//
+//  Created by MAHESHWARAN on 21/06/23.
+//
+
+import SwiftUI
+
+/// `TUISelectionRow` is a  container view that displays any view with selection color
+///  The content can be any SwiftUI view.
+///
+/// Example usage:
+///
+///     TUISelectionRow {
+///       HStack {
+///         Image(Symbol.map)
+///         Text("Description")
+///           .foregroundColor(Color.surface)
+///
+///         Spacer()
+///       }
+///     }
+///
+/// - Parameters:
+///   - isSelected: This Bool is used to display the selection, The default selection color is `.surface`
+///
+/// - Returns: A closure that returns the content
+
+public struct TUISelectionRow<Content>: View where Content: View {
+  
+  var content: () -> Content
+  
+  public var isSelected: Bool
+  
+  public init(isSelected: Bool = false,
+              @ViewBuilder content: @escaping () -> Content) {
+    self.isSelected = isSelected
+    self.content = content
+  }
+  
+  public var body: some View {
+    HStack {
+      content()
+    }
+    .frame(maxWidth: .infinity)
+    .padding(.leading)
+    .padding(.vertical, Spacing.baseVertical)
+    .background(isSelected ? Color.primaryAlt : Color.surface)
+    .clipShape(RoundedRectangle(cornerRadius:Spacing.baseHorizontal))
+    .accessibilityIdentifier(Accessibility.root)
+  }
+}
+
+extension TUISelectionRow {
+  enum Accessibility: String, TUIAccessibility {
+    case root = "TUISelectionRow"
+  }
+}
+
+struct TUISelectionRow_Previews: PreviewProvider {
+  static var previews: some View {
+    VStack {
+      TUISelectionRow {
+        TUITextRow("Hello", style: .textDescription("Welcome"))
+      }
+      TUISelectionRow(isSelected: true) {
+        TUITextRow("Hello", style: .textDescription("Welcome"))
+      }
+    }
+  }
+}
