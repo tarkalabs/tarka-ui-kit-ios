fd6c59a06d4fe74ade213157df001dc7c6251d1b
Gopinath Raveendran <gopinath@eam360.com>
2023-06-28 16:17:25 +0530

    interaction enabled property introduced for TUIWrapperIcon

---
+++ Sources/TarkaUI/Modifiers/TUIWrapperIcon+Modifiers.swift
@@ -0,0 +1,23 @@
+//
+//  TUIWrapperIcon+Modifiers.swift
+//  
+//
+//  Created by Gopinath on 28/06/23.
+//
+
+import SwiftUI
+
+public extension TUIWrapperIcon {
+  
+  func iconColor(_ color: Color) -> TUIWrapperIcon {
+    var newView = self
+    newView.color = color
+    return newView
+  }
+  
+  func disableInteraction(_ disableInteraction: Bool = true) -> TUIWrapperIcon {
+    var newView = self
+    newView.isInteractionEnabled = !disableInteraction
+    return newView
+  }
+}
