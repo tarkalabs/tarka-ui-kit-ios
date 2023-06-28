022c6c4114a9f6080302c3d8f44de52dd287709f
Gopinath Raveendran <gopinath@eam360.com>
2023-06-28 17:58:32 +0530

    TUIInputFiled renaming

--- Sources/TarkaUI/Modifiers/TUIInputText+Modifiers.swift
+++ Sources/TarkaUI/Modifiers/TUIInputText+Modifiers.swift
@@ -7,27 +7,27 @@
 
 import SwiftUI
 
-public extension TUIInputText {
+public extension TUIInputField {
   
-  func startItem(show: Bool = true, withStyle style: TUIInputAdditionalItem.Style) -> TUIInputText {
+  func startItem(show: Bool = true, withStyle style: TUIInputAdditionalItem.Style) -> TUIInputField {
     var newView = self
     newView.startItemStyle = show ? style : nil
     return newView
   }
   
-  func endItem(show: Bool = true, withStyle style: TUIInputAdditionalItem.Style) -> TUIInputText {
+  func endItem(show: Bool = true, withStyle style: TUIInputAdditionalItem.Style) -> TUIInputField {
     var newView = self
     newView.endItemStyle = show ? style : nil
     return newView
   }
 
-  func highlightBar(_ show: Bool = true) -> TUIInputText {
+  func highlightBar(_ show: Bool = true) -> TUIInputField {
     var newView = self
     newView.showHighlightBar = show
     return newView
   }
   
-  func helperText(show: Bool = true, @ViewBuilder _ helperText: () -> TUIHelperText) -> TUIInputText {
+  func helperText(show: Bool = true, @ViewBuilder _ helperText: () -> TUIHelperText) -> TUIInputField {
     var newView = self
     newView.helperText = show ? helperText() : nil
     return newView
