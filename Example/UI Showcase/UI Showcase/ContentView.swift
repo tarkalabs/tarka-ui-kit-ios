1d4789b948bd103994103dfaecf927d711d33c87
Gopinath Raveendran <gopinath@eam360.com>
2023-07-06 12:45:20 +0530

    Previews reverted

--- Example/UI Showcase/UI Showcase/ContentView.swift
+++ Example/UI Showcase/UI Showcase/ContentView.swift
@@ -9,32 +9,17 @@ import TarkaUI
 import SwiftUI
 
 struct ContentView: View {
+  
   var body: some View {
+    
     VStack {
-      TUIInputField(style: .titleWithValue(title: "Label", value: "Input Text"))
-        .startItem(withStyle: .text("Title:"))
-        .endItem(withStyle: .text("$"))
-      
-      TUIInputField(style: .titleWithValue(title: "Label", value: "Input Text"))
-        .startItem(withStyle: .icon(Symbol.sync))
-        .endItem(withStyle: .icon(Symbol.sync))
-      
-      TUIInputField(style: .titleWithValue(title: "Label", value: "Input Text"))
-        .startItem(withStyle: .text("Title:"))
-        .endItem(withStyle: .icon(Symbol.sync))
-      
-      TUIInputField(style: .titleWithValue(title: "Label", value: "Input Text"))
-        .startItem(withStyle: .icon(Symbol.sync))
-        .endItem(withStyle: .text("$"))
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")
+      Image(Symbol.map)
     }
-//    VStack {
-//      Image(systemName: "globe")
-//        .imageScale(.large)
-//        .foregroundColor(.accentColor)
-//      Text("Hello, world!")
-//      Image(Symbol.map)
-//    }
-//    .padding()
+    .padding()
   }
 }
 
