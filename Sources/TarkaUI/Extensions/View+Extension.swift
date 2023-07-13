d98406c4dd134d387cd1e55da4853f6a5f69b87f
Gopinath Raveendran <gopinath@eam360.com>
2023-07-13 15:59:56 +0530

    TUIButton component has been introduced

---
+++ Sources/TarkaUI/Extensions/View+Extension.swift
@@ -0,0 +1,35 @@
+//
+//  View+Extension.swift
+//  
+//
+//  Created by Gopinath on 13/07/23.
+//
+
+import SwiftUI
+
+extension View {
+  
+  @ViewBuilder
+  func isEnabled<Content: View>(_ show: Bool,
+                                content: (Self) -> Content) -> some View {
+    if show {
+      content(self)
+    } else {
+      self
+    }
+  }
+  
+  @ViewBuilder
+  func roundedCorner(width: CGFloat, color: Color) -> some View {
+    if width > 0 {
+      self.overlay(
+        RoundedRectangle(cornerRadius: .infinity)
+          .stroke(color, lineWidth: width)
+      )
+    } else {
+      self
+        .cornerRadius(.infinity)
+        .border(color, width: width)
+    }
+  }
+}
