92740bbd0c0ec64f2a87796cd59b558ad42a30e0
Gopinath Raveendran <gopinath@eam360.com>
2023-09-01 12:24:16 +0530

    Input Field types defined according to attribute field type

--- Example/UI Showcase/EAM/Specifications/AddSpecItem.swift
+++ Example/UI Showcase/EAM/Specifications/AddSpecItem.swift
@@ -36,22 +36,31 @@ struct AddSpecItem {
   var sections: [Section]
 }
 
-struct SpecificationItem {
-  var value: String
-  var section: String
-  @Binding var specAttributes: [SpecAttributeItem]
-}
-
 class SpecAttributeItem: ObservableObject, Identifiable {
   
   public var id: String {
     return UUID().uuidString
   }
 
-  var inputFieldItem: TUIInputFieldItem
+  enum FieldType {
+    case lookup, date, numeric, alphaNumeric
+  }
+
+  var name: String
+  var section: String
+  var fieldType: FieldType
   
-  init(attributeName: String) {
-    self.inputFieldItem = TUIInputFieldItem(style: .onlyTitle, title: attributeName)
+  var inputFieldItem: TUIInputFieldItem
+  var dateInputFieldItem: TUIDateInputFieldItem
+
+  init(name: String, section: String, fieldType: FieldType) {
+    self.name = name
+    self.section = section
+    self.fieldType = fieldType
+    self.inputFieldItem = TUIInputFieldItem(style: .onlyTitle, title: name)
+    self.dateInputFieldItem = TUIDateInputFieldItem(
+      style: .onlyTitle, title: name,
+      format: .init(date: .abbreviated, time: .standard))
   }
 }
 
@@ -59,32 +68,30 @@ class SpecificationViewModel: ObservableObject {
   
   @Published var addSpecItem: AddSpecItem
   
-  var allSpecs: [SpecificationItem] = {
-    let specsForSection: (String) -> [SpecificationItem] = { section in
-      let spec1 = SpecificationItem(
-        value: "FullScale \(section)", section: section, specAttributes: attrs)
-      let spec2 = SpecificationItem(
-        value: "HalfScale \(section)", section: section, specAttributes: attrs)
-      return [spec1, spec2]
-    }
+  var allSpecAttributes: Binding<[SpecAttributeItem]> = {
     
-    var attrs: Binding<[SpecAttributeItem]> {
-      let attr1 = SpecAttributeItem(attributeName: "Date")
-      let attr2 = SpecAttributeItem(attributeName: "Time")
-      let attr3 = SpecAttributeItem(attributeName: "Frequency")
-      return Binding.constant([attr1, attr2, attr3])
+    let specsForSection: (String) -> [SpecAttributeItem] = { section in
+      let attr1 = SpecAttributeItem(
+        name: "FullScale \(section)", section: section, fieldType: .lookup)
+      let attr2 = SpecAttributeItem(
+        name: "Date", section: section, fieldType: .date)
+      let attr3 = SpecAttributeItem(
+        name: "Time", section: section, fieldType: .numeric)
+      let attr4 = SpecAttributeItem(
+        name: "Frequency", section: section, fieldType: .alphaNumeric)
+      return [attr1, attr2, attr3, attr4]
     }
-    
+   
     let specsOfSectionA = specsForSection("Section A")
     let specsOfSectionB = specsForSection("Section B")
     let specsOfSectionC = specsForSection("Section C")
-    return [specsOfSectionA, specsOfSectionB, specsOfSectionC].flatMap { $0 }
+    return .constant([specsOfSectionA, specsOfSectionB, specsOfSectionC].flatMap { $0 })
   }()
   
   init() {
     
-    let sections = allSpecs
-      .compactMap({ $0.section })
+    let sections = allSpecAttributes
+      .compactMap({ $0.wrappedValue.section })
       .removingDuplicates()
       .compactMap({ AddSpecItem.Section.value($0) })
 //    if !sections.isEmpty {
@@ -98,41 +105,39 @@ class SpecificationViewModel: ObservableObject {
       description: "Short description goes here", sections: sections)
   }
   
-  func fetchSpecificationForSection(_ section: AddSpecItem.Section, searchText: String? = nil) -> [SpecificationItem] {
+  func fetchSpecificationForSection(_ section: AddSpecItem.Section, searchText: String? = nil) -> Binding<[SpecAttributeItem]> {
     
     let searchWords: [String] = [searchText?.lowercased() ?? ""]//searchText?.lowercased().split(separator: " ").map({ String($0) })
-    var filteredSpecs = [SpecificationItem]()
+    var filteredSpecAttrs = [SpecAttributeItem]()
     
-    let isFiltered: (SpecificationItem) -> Bool = { spec in
+    let isFiltered: (SpecAttributeItem) -> Bool = { spec in
       
-      let searchableValue = spec.value
+      let searchableValue = spec.name
       let searchList = searchWords.first(where: {
         guard !$0.isEmpty else { return true }
         return searchableValue.lowercased().contains($0.lowercased())
       })
       return searchList != nil
     }
-    
     switch section {
       
     case .none:
-      filteredSpecs = allSpecs.filter({
-        
+      filteredSpecAttrs = allSpecAttributes.wrappedValue.filter({
         let isEmpty = $0.section.isEmpty
         guard isEmpty else  { return false }
         guard !searchWords.isEmpty else  { return isEmpty }
         return isFiltered($0)
       })
-                                      
+      
     case .all:
-      filteredSpecs = allSpecs.filter({
+      filteredSpecAttrs = allSpecAttributes.wrappedValue.filter({
         
         guard !searchWords.isEmpty else  { return true }
         return isFiltered($0)
       })
       
     case .value(let value):
-      filteredSpecs = allSpecs.filter({
+      filteredSpecAttrs = allSpecAttributes.wrappedValue.filter({
         
         let isSameSection = $0.section == value
         guard isSameSection else  { return false }
@@ -140,7 +145,7 @@ class SpecificationViewModel: ObservableObject {
         return isFiltered($0)
       })
     }
-    return filteredSpecs
+    return .constant(filteredSpecAttrs)
   }
 }
 
