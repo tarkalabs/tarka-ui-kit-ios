7cd9bc4ba1e247a2ec27895c548f925b4bb90535
MAHESHWARAN <maheshwaran@eam360.com>
2023-12-13 11:32:49 +0530

    TUISearchBar  - Moved perform search to TUISearchTextField

--- Sources/TarkaUI/Helper Views/LazyView.swift
+++ Sources/TarkaUI/Helper Views/LazyView.swift
@@ -7,6 +7,25 @@
 
 import SwiftUI
 
+/// LazyView is a SwiftUI
+///
+/// The `LazyView` is a container view that displays a contents only when a view is visible,
+/// This lazyView will solve the issue where nested views are getting initialized too early.
+///
+/// Example usage:
+///
+///     LazyView {
+///       HStack {
+///         Text("Description")
+///           .font(.heading6)
+///           .foregroundColor(Color.inputText)
+///       }
+///     }
+///
+/// - Parameters:
+///   - build: A closure that returns the content
+///
+/// - Returns: A view that represents a content.
 public struct LazyView<V: View>: View {
   public let build: () -> V
   
