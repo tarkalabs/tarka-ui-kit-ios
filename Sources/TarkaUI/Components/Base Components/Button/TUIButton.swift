0520209750aad17c167d7577bb972fa4ea035f47
MAHESHWARAN <102943217+maheshwaran01m@users.noreply.github.com>
2024-03-28 01:05:17 +0530

    Added Support for customStyle in TUIButton+Style (#110)
    * TUIButton+Style
     - Added customStyle
    
    * TUIIconButton
     - Added custom style
    
    * TUIIconButton
     - Updated style
    
    * TUIButton
     - Handled custom background color
    
    * TUIButton
     - Updated icon color
    
    * TUIButton
     - Reverted to customStyle
    
    * TUIButton
     - Handled `isPressed`
    
    * TUIButton
     - Handled `isPressed`
    
    * TUIButton+Style
     - Updated border color
    
    * TUIIconButton
     - Updated with `ceil` for fractional values
    
    * TUIconButton
     - Removed ceil
    
    * TUIButton
     - Updated external parameter
    
    * TUIButton
     - Updated border width

--- Sources/TarkaUI/Components/Base Components/Button/TUIButton.swift
+++ Sources/TarkaUI/Components/Base Components/Button/TUIButton.swift
@@ -43,7 +43,7 @@ public struct TUIButton: View {
         
         Text(title)
           .font(size.buttonSize)
-          .foregroundColor(style.foregroundColor)
+          .foregroundColor(style.inputStyle.foreground)
           .padding(.vertical, size.titleTopPadding)
           .frame(minHeight: size.titleHeight)
         
@@ -64,7 +64,7 @@ public struct TUIButton: View {
     Image(fluent: icon)
       .scaledToFit()
       .frame(width: size.iconSize, height: size.iconSize)
-      .foregroundColor(style.foregroundColor)
+      .foregroundColor(style.inputStyle.foreground)
       .clipped()
   }
   
@@ -75,8 +75,9 @@ public struct TUIButton: View {
     func makeBody(configuration: Configuration) -> some View {
       configuration.label
         .frame(minHeight: size.height)
-        .background(style.backgroundColor)
-        .border(Capsule(), width: style.borderWidth, color: .onSurface)
+        .background(style.inputStyle.background)
+        .border(Capsule(), width: style.borderWidth(configuration.isPressed),
+                color: style.borderColor(configuration.isPressed))
     }
   }
 }
@@ -94,7 +95,9 @@ struct TUIButton_Previews: PreviewProvider {
         let isLargeIcon: Bool = size == .large || size == .regular
         let icon: FluentIcon = isLargeIcon ? .add24Regular : .add16Regular
         
-        ForEach(TUIButton.Style.allCases) { style in
+        let buttons = [TUIButton.Style.primary, .secondary, .danger, .outlined, .ghost]
+        
+        ForEach(buttons) { style in
           
           HStack(spacing: 10.0) {
             
@@ -108,7 +111,7 @@ struct TUIButton_Previews: PreviewProvider {
               .icon(.right(icon))
             
             TUIButton(title: "Label") { }
-              .style(style)
+              .style(.custom(.init(background: .accentBaseA, foreground: .onAccentBaseA, border: .accentBaseA)))
               .size(size)
               .icon(.left(icon))
           }
