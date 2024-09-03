ebb46a2770012439d0dfbfd8000af5a8b517e77e
Sunil <sunil@eam360.com>
2024-09-03 12:06:06 +0530

    made the TUIHelperText public to access it outside TarkaUI files.

--- Sources/TarkaUI/Components/Input Field/Model/TUIInputFieldState.swift
+++ Sources/TarkaUI/Components/Input Field/Model/TUIInputFieldState.swift
@@ -49,35 +49,22 @@ public enum TUIInputFieldState: Equatable {
   
   case none
   case focused
-  case inactive(String, showIcon: Bool = true)
-  case error(String, showIcon: Bool = true)
-  case alert(String, showIcon: Bool = true)
-  case success(String, showIcon: Bool = true)
-  case disabled(String, showIcon: Bool = true)
+  case inactive(String)
+  case error(String)
+  case alert(String)
+  case success(String)
+  case disabled(String)
   
   private var message: String? {
     switch self {
     case .none, .focused:
       return nil
-    case .inactive(let message, _),
-        .error(let message, _),
-        .alert(let message, _),
-        .success(let message, _),
-        .disabled(let message, _):
+    case .inactive(let message),
+        .error(let message),
+        .alert(let message),
+        .success(let message),
+        .disabled(let message):
       return message
-    }
-  }
-  
-  private var showIcon: Bool {
-    switch self {
-    case .none, .focused:
-      return false
-    case .inactive(_, let showIcon),
-        .error(_, let showIcon),
-        .alert(_, let showIcon),
-        .success(_, let showIcon),
-        .disabled(_, let showIcon):
-      return showIcon
     }
   }
   
@@ -93,13 +80,13 @@ public enum TUIInputFieldState: Equatable {
     guard let message else { return nil }
     switch self {
     case .error:
-      return TUIHelperText(style: .error, message: message, showIcon: showIcon)
+      return TUIHelperText(style: .error, message: message)
     case .alert:
-      return TUIHelperText(style: .warning, message: message, showIcon: showIcon)
+      return TUIHelperText(style: .warning, message: message)
     case .success:
-      return TUIHelperText(style: .success, message: message, showIcon: showIcon)
+      return TUIHelperText(style: .success, message: message)
     default:
-      return TUIHelperText(style: .hint, message: message, showIcon: showIcon)
+      return TUIHelperText(style: .hint, message: message)
     }
   }
 }
