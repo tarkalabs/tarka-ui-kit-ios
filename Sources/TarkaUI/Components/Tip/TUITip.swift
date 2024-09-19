75a2f3cc030d6e8aec3eb93fa00a99f27336156b
Navin Kumar <navin@eam360.com>
2024-09-19 15:53:46 +0530

    implemented tipView modifier

--- Sources/TarkaUI/Components/Tip/TUITip.swift
+++ Sources/TarkaUI/Components/Tip/TUITip.swift
@@ -27,16 +27,54 @@ public extension Tip where Self: TUITip {
   
 }
 
+/// A SwiftUI modifier that conditionally applies a TipKit tip for iOS 17+.
+/// Optimized to prevent re-render issues by encapsulating tip logic in the modifier.
+/// The tip is only displayed if `tuiTip` is available and running on iOS 17+.
+///
+/// Example usage:
+/// ```swift
+/// let myTip = MyTip() // Conforms to TUITip
+/// someView.modifier(TipViewModifier(myTip, arrowEdge: .top))
+/// ```
+struct TipViewModifier: ViewModifier {
+  
+  let tuiTip: TUITip?
+  let arrowEdge: Edge
+  
+  init(_ tuiTip: TUITip?, arrowEdge: Edge) {
+    self.tuiTip = tuiTip
+    self.arrowEdge = arrowEdge
+  }
+  
+  func body(content: Content) -> some View {
+    mainView(content)
+  }
+  
+  @ViewBuilder
+  func mainView(_ content: Content) -> some View {
+    if #available(iOS 17, *), let tip = tuiTip?.tip {
+      content.popoverTip(tip, arrowEdge: arrowEdge)
+    } else {
+      content
+    }
+  }
+}
 
 public extension View {
-  
-  /// Helper for making the `popupTip` modifier available for iOS <17.
+  /// /// A helper for applying the `popupTip` modifier for iOS versions below 17.
+  ///
+  /// - Parameters:
+  ///   - tip: The optional `TUITip` instance to use.
+  ///   - arrowEdge: The edge where the arrow of the tip should point.
+  /// - Returns: A view with the `popupTip` modifier applied for iOS < 17.
+  ///
+  /// Example usage:
+  /// ```swift
+  /// let myTip = MyTip() // Example tip
+  /// someView.popupTip(myTip, arrowEdge: .top)
+  /// ```
   @available(iOS, obsoleted: 17, message: "Can be removed once we only support iOS 17+")
   @ViewBuilder func popupTip(_ tip: TUITip?, arrowEdge: Edge = .top) -> some View {
-    if #available(iOS 17, *), let tip = tip?.tip {
-      self.popoverTip(tip, arrowEdge: arrowEdge)
-    } else {
-      self
-    }
+    modifier(TipViewModifier(tip, arrowEdge: arrowEdge))
   }
 }
