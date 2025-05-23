46b0c01c882672574b9bd74d6a4271813d4e5fb3
Gopinath Raveendran <gopinath@eam360.com>
2025-05-23 22:26:05 +0530

    Kingfisher version has been upgraded to resolve library issue

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     // .package(url: /* package url */, from: "1.0.0"),
     // `JustifiableFlowLayout` is used in components like `TUIEmailField`, so that the items can flow in one direction in a grid
     // with equal spacing and height, and wrap to the next line when needed
-    .package(url: "https://github.com/lorin-vr/JustifiableFlowLayout", from: "1.0.0"),
-    .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0")
+    .package(url: "https://github.com/lorin-vr/JustifiableFlowLayout", exact: "1.0.0"),
+    .package(url: "https://github.com/onevcat/Kingfisher.git", exact: "7.6.2")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
