f074cbfb2f89ffd37648e3f711dd81ee9d353934
Gopinath Raveendran <gopinath@eam360.com>
2025-11-21 10:45:14 +0530

    Used @Entry for EnvironmentValues

---
+++ Sources/TarkaUI/Extensions/EnvironmentValues+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  EnvironmentValues+Extensions.swift
+//  EAM360
+//
+//  Created by Gopinath on 20/11/25.
+//  Copyright © 2025 Sedin Technologies. All rights reserved.
+//
+
+import SwiftUI
+
+public extension EnvironmentValues {
+  @Entry var tuiSafeAreaInsets: EdgeInsets = .init()
+}
